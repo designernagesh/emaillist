47a6aba07e82fedad3be938bfa29ac0a760ee284
Nagesh <nagesh.k@tecnolynx.com>
2020-02-10 10:44:25 +0530

    Updates

---
+++ precache-manifest.92932cdb7229d106ca936631272487e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d35b0f6cca771920a3ec83f17195fda",
+    "url": "/emaillist/index.html"
+  },
+  {
+    "revision": "c1db066e91e61129d855",
+    "url": "/emaillist/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0a4acc216bf88558c8f6",
+    "url": "/emaillist/static/js/2.a9ed1bfe.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/emaillist/static/js/2.a9ed1bfe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1db066e91e61129d855",
+    "url": "/emaillist/static/js/main.53024099.chunk.js"
+  },
+  {
+    "revision": "70d763b5dbb100671cc2",
+    "url": "/emaillist/static/js/runtime-main.3bc19acd.js"
+  }
+]);
